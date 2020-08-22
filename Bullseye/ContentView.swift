cdbc6fe1e82a897feb977afdabdfb8c4dac39250
safarsafarov <safarov.safar@hotmail.com>
2020-08-23 00:17:30 +0800

    Bullyseye-ios13

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Safar Safarov on 2020/8/22.
+//  Copyright © 2020 Safarov Safar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to my not first app")
+            .fontWeight(.black)
+            .foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
