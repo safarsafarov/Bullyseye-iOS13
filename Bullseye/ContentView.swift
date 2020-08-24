dc013528add64825ce72d77f4bce9a9eceb96a5e
safarsafarov <safarov.safar@hotmail.com>
2020-08-25 03:30:21 +0800

    Bullseye-ios13

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,12 +11,15 @@ import SwiftUI
 struct ContentView: View {
     // Properties
     // ==========
-    
-    // User interface views
-    @State var alertIsVisible: Bool = false
-    @State var sliderValue: Double = 50.0
-    @State var target: Int = Int.random(in: 1...100)
-    
+
+    // User interface view
+    @State var alertIsVisible = false
+    @State var sliderValue = 50.0
+    @State var target = Int.random(in: 1...100)
+    var sliderValueRounded: Int {
+        Int(self.sliderValue.rounded())
+    }
+    @State var score = 0
     // User interface content and layout
     var body: some View {
         VStack {
@@ -26,77 +29,74 @@ struct ContentView: View {
                 Text("Put the bullseye as close as you can to:”")
                 Text("100")
             }
-            
+
             // Slider row
-            HStack{
+            HStack {
                 Text("1")
                 Slider(value: self.$sliderValue, in: 1...100)
                 Text("100")
             }
-            
+
             Spacer()
             // TODO: Add views for the slider row here.
-            
+
             // Button row
-           // Button row
-           Button(action: {
-            print("Points awarded: \(self.pointsForCurrentRound())")
-            self.alertIsVisible = true
-           }) {
-            Text("Hit me!")
-           }
-           alert(isPresented: self.$alertIsVisible) {
-            Alert(title: Text("Hello there!"),
-            message: Text("The slider's value is \(Int(self.sliderValue.rounded())).\n" + "The target value is \(self.target).\n" + "You scored \(pointsForCurrentRound())points this round."),
-            dismissButton: .default(Text("Awesome!")))
-           }
-            
+            Button(action: {
+                print("Points awarded: \(self.pointsForCurrentRound())")
+                self.alertIsVisible = true
+                self.score = self.score + self.pointsForCurrentRound()
+            }) {
+                Text("Hit me!")
+            }
+                    .alert(isPresented: self.$alertIsVisible) {
+                        Alert(title: Text("Hello there!"),
+                                message: Text(self.scoringMessage()),
+                                dismissButton: .default(Text("Awesome!")))
+                    }
+
             Spacer()
-            
+
             // Score row
-           HStack {
-             Button(action: {}) {
-                Text("Start over")
-             }
-             Spacer()
-             Text("Score:")
-             Text("999999")
-             Spacer()
-             Text("Round:")
-             Text("999")
-             Spacer()
-             Button(action: {}) {
+            HStack {
+                Button(action: {}) {
+                    Text("Start over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {}) {
                     Text("Info")
                 }
             }
 
-           .padding(.bottom, 20)
+                    .padding(.bottom, 20)
             // TODO: Add views for the score, rounds, and start info buttons here.
         }
     }
     // Methods
     // =======
-  func pointsForCurrentRound() -> Int {
-    let sliderValueRounded = Int(self.sliderValue.rounded())
-    let difference: Int
-    if sliderValueRounded > self.target {
-        difference = sliderValueRounded - self.target
-    } else if self.target > sliderValueRounded {
-        difference = self.target - sliderValueRounded
-    } else {
-        difference = 0
+    func pointsForCurrentRound() -> Int {
+        let maximumScore = 100
+        let difference = abs(self.sliderValueRounded - self.target)
+        return maximumScore - difference
     }
-    return 100 - difference
-   }
-    
-}
 
+    func scoringMessage() -> String {
+        return "The slider's value is \(self.sliderValueRounded).\n" +
+                "The target value is \(self.target).\n" +
+                "You scored \(self.pointsForCurrentRound()) points this round."
+        }
 // Preview
 // =======
-#if DEBUG
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    #if DEBUG
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+    #endif
 }
-#endif
